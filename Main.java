cb1390804ea2fb065c1c8bf3b82f9bfaf78d16ed
asemnurlan <asemnurlanov@gmail.com>
2025-01-24 10:53:33 +0600

    connection
    Co-Authored-By: Nurassyl Maratov <51500758+123132123123@users.noreply.github.com>
    Co-Authored-By: Matias447 <186990580+Matias447@users.noreply.github.com>
    Co-Authored-By: Zhalgastelmanzz <185967632+Zhalgastelmanzz@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 import dao.*;
 import classes.*;
+import database.IDB;
+import database.PostgresConnection;
+
+import java.sql.Connection;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        IDB db = new PostgresConnection("jdbc:postgresql://localhost:5432", "postgres", "0000", "aviatickets");
 
     }
 }
